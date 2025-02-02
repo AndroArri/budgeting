55d997db18cf788cc9f5c7b096ef24903bb1c716
Andrea <a.arrighini93@gmail.com>
2025-02-02 13:07:25 +0100

    feat: Replace span with button in Button.vue for improved semantics

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,16 +1,19 @@
+
 <template>
-  <span
+  <button
     :class="[
-      'inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-medium',
-      variant === 'primary' && 'bg-green-100 text-green-800',
+      'inline-flex items-center rounded-full px-2.5 py-0.5 text-sm font-semibold text-white shadow-sm',
+      variant === 'primary' &&
+        'block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white hover:bg-indigo-500',
       variant === 'abort' && 'bg-red-100 text-red-800',
       variant === 'warning' && 'bg-yellow-100 text-yellow-800',
       variant === 'info' && 'bg-blue-100 text-blue-800',
       variant === 'secondary' && 'bg-gray-100 text-gray-800',
     ]"
   >
+    <slot name="icon-left"></slot>
     <slot></slot>
-  </span>
+  </button>
 </template>
 
 <script setup lang="ts">
