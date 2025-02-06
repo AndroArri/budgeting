1706ecedf35d3ebcdd88982eca107c5971cf5c67
Andrea <a.arrighini93@gmail.com>
2025-02-06 19:39:28 +0100

    feat: Add path aliasing in Vite configuration for improved imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "/src/assets"),
+      "@components": path.resolve(__dirname, "/src/components"),
+      "@services": path.resolve(__dirname, "/src/services"),
+      "@types": path.resolve(__dirname, "/src/types"),
+      "@": path.resolve(__dirname, "/src"),
+      "@views": path.resolve(__dirname, "/src/views"),
+    },
+  },
+});
